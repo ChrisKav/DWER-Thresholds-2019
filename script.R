a65234bd65c0b09f4d8ef13f281c0c702137e475
ChrisKav <ck855@uowmail.edu.au>
2019-09-12 17:51:01 +1200

    Added non-veg sites

--- script.R
+++ script.R
@@ -17,12 +17,12 @@ for (i in 1:length(wetlands)) {
   source(paste0(wetlands[i], "/water_summary.R"))
 }
 
-#mcmc_control <- list(n.burnin = 10000, n.iteration = 60000, 
-#                     n.thin = 50, seed=28041948)
-
 #######################
 # Vegetaton analysis  #
 #######################
+
+#mcmc_control <- list(n.burnin = 10000, n.iteration = 60000, 
+#                     n.thin = 50, seed=28041948)
 
 mcmc_control <- list(n.burnin = 10, n.iteration = 60, 
                      n.thin = 5, seed=28041948)
