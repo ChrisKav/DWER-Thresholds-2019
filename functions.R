7061916d86133fa3d064273f4e9ac5db94dbe867
ChrisKav <ck855@uowmail.edu.au>
2019-10-09 05:43:46 +1300

    Added read_excel_allsheets to functions.R

--- functions.R
+++ functions.R
@@ -226,3 +226,10 @@ stratiplotter <- function(x) {
     facet_grid(df2$plot~df2$variable) #,scales = "free") #space = "free")
   return(strat_plot)
 }
+
+read_excel_allsheets <- function(filename) {
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  names(x) <- sheets
+  x
+}
